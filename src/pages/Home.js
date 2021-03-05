829c91709dfa65c5f1f061d07921a23e59a3d8f4
joo <sometales7@gmail.com>
2021-03-05 21:01:56 +0900

    학습및 json-server 적용
    - json-server 적용
    - setState 예제적용

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Home = () => (
-    <div>
-        This page is Home
-    </div>
-);
+class Home extends Component {
+    constructor(props){
+        super(props)
+        this.launchClock()
+        this.state = {currentTime: (new Date()).toLocaleString('en')}
+    }
+    launchClock(){
+        setInterval(()=>{
+            console.log('Updating time...')
+            this.setState({
+                currentTime: (new Date()).toLocaleString('en')
+            })
+        }, 1000)
+    }
+    render() {      
+        return (
+            <div>
+                This page is Home<br/>
+                {this.state.currentTime}
+            </div>
+        );
+    };
+};
+
 
 export default Home;
